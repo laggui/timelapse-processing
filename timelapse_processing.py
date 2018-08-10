a9d6bbe573818edae1102a90bb1c3a1e91bccb51
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2018-08-10 14:08:26 -0400

    Added toRGB function and histMatch function definition

--- timelapse_processing.py
+++ timelapse_processing.py
@@ -5,6 +5,17 @@ from collections.abc import MutableSequence
 def loadImage(path):
     return cv2.imread(path, 1)
 
+def toRGB(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def histMatch(sourceImage, expImage):
+    '''
+    Matches the histogram of expImage to sourceImage in order to fix lightness/exposure
+    of the expImage.
+    '''
+    expImage = sourceImage
+    return expImage
+
 class Image:
     '''
     An Image object.
@@ -14,8 +25,8 @@ class Image:
         self.lightness = self._getLightness()
     
     def _getLightness(self):
-        lab_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2LAB)
-        l_channel,_,_ = cv2.split(lab_img)
+        lab = cv2.cvtColor(self.img, cv2.COLOR_BGR2LAB)
+        l_channel,_,_ = cv2.split(lab)
         return int(np.mean(l_channel))
 
 class ImageList(MutableSequence):
@@ -24,32 +35,39 @@ class ImageList(MutableSequence):
     as well as statistics on the lightness of the images in the list such as the median and 
     median absolute deviation (mad). These stats are useful for processing.
     '''
+    @staticmethod
+    def __checkValue(value):
+        if not isinstance(value, Image):
+            raise TypeError('Invalid instance passed when trying to set item value.')
+    
     def __init__(self):
-        self._imglist = []
+        self._imgList = []
         self.lightMed = 0
         self.lightMad = 0
     
     def computeStats(self):
-        lightness = np.array((img.lightness for img in self._imglist))
+        lightness = np.array((img.lightness for img in self._imgList))
         self.lightMed = np.median(lightness)
         self.lightMad = np.median(abs(lightness - self.lightMed))
     
-    def __checkValue(value):
-        if not isinstance(value, Image):
-            raise TypeError()
+    def fixExposure(self):
+        for i, img in enumerate(self._imgList):
+            if (img.lightness < (self.lightMed - self.lightMad) or 
+                img.lightness > (self.lightMed + self.lightMad)):
+                self._imgList[i].img = histMatch(self._imgList[i-1].img, img)
     
     def __len__(self):
-        return len(self._imglist)
+        return len(self._imgList)
 
     def __getitem__(self, index):
-        return self._imglist[index]
+        return self._imgList[index]
 
     def __delitem__(self, index):
-        del self._imglist[index]
+        del self._imgList[index]
 
     def __setitem__(self, index, value):
         self.__checkValue(value)
-        self._imglist[index] = value
+        self._imgList[index] = value
     
     def insert(self, index, value):
-        self._imglist.insert(index, value)
+        self._imgList.insert(index, value)
